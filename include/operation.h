474be9a69a1d63bca19b3dbace923dbd992ccbd9
Loic COZDENMAT <lcozdenm@e3r7p8.clusters.42paris.fr>
2023-03-06 20:48:01 +0100

    starting documentation

--- include/operation.h
+++ include/operation.h
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 11:20:21 by lcozdenm          #+#    #+#             */
-/*   Updated: 2023/03/06 15:53:02 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2023/03/06 20:46:31 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,17 +43,55 @@ typedef struct s_env
 }	t_env;
 
 /** OPERATIONS */
+/* Do an operation on the stacks 
+ * Return -1 if an error occured
+ * Return 0 if the operation was successful
+ * Return the operation if the operation was not successful
+ */
 int		do_operation(t_env *env, t_operation op);
+
+/* Return the string representation of an operation */
 char	*op_to_str(t_operation op);
+
+/* Swap the first two elements of a stack */
 void	swap_stack(t_stack **stack);
+
+/* Push the first element of stack b to stack a */
 void	push_stack(t_stack **st_a, t_stack **st_b);
+
+/* Rotate the stack to the top */
 void	rotate_stack(t_stack **stack, int size);
+
+/* Rotate the stack to the bottom */
 void	reverse_stack(t_stack **stack, int size);
-t_env	*lil_sort(t_env *env);
-t_env	*five_sort(t_env *env);
+
 /** PUSH SWAP **/
+/* Initialize the environment */
 t_env	*init_env(int *array, int size);
+
+/* Free the environment */
 void	free_env(t_env *env);
+
+/* Sort the stack using the radix algrithm, offset
+   is telling which bit to sort */
 t_env	*radix(t_env *env, int offset);
-t_env	*loic_sort(t_env *env, int bitsize);
+
+/* Sort the stack using the loic algorithm,
+   either a lil_sort if the size is equal to 3,
+   or a five_sort if the size is equal to 5,
+   or a radix sort if the size is greater than 5 */
+t_env	*loic_sort(t_env *env, int bitsize);\
+
+/* Sort the stack if the size is equal to 3 */
+t_env	*lil_sort(t_env *env);
+
+/* Sort the stack if the size is equal to 5,
+   push the 2 biggest elements to stack b before doing
+   a lil_sort on stack a */
+t_env	*five_sort(t_env *env);
+
+/** UTILITIES **/
+/* Tell you if the array of length size is sorted or not */
+t_bool	is_sorted(int *array, int size);
+
 #endif
--- include/stack.h
+++ include/stack.h
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 11:19:19 by lcozdenm          #+#    #+#             */
-/*   Updated: 2023/03/06 15:47:36 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2023/03/06 20:37:42 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,11 @@ typedef struct s_stack
 	struct s_stack	*prev;
 }	t_stack;
 
+/** STACK **/
 t_stack	*new_stack(int *data, int size);
 t_stack	*add_stack(t_stack **stack, int data);
 void	del_stack(t_stack **stack);
 void	free_stack(t_stack **stack, int size);
 void	print_stack(t_stack *stack, t_bool reverse);
-int		get_bmax(int *array, int size);
 
 #endif
